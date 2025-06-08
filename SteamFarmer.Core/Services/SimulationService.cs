230275273a2c7e8c178911ed58b8ef3ccc9210c1
mmm1008fm <44507247+mmm1008fm@users.noreply.github.com>
2025-06-08 14:02:17 +0300

    work in progress

--- SteamFarmer.Core/Services/SimulationService.cs
+++ SteamFarmer.Core/Services/SimulationService.cs
@@ -1,43 +1,52 @@
-using Facepunch.Steamworks;
-using SteamFarmer.Core.Services;
+using Steamworks;
 using System;
 using System.Threading;
 
 namespace SteamFarmer.Core.Services
 {
-    public class SimulationService : ISimulationService
+    public class SimulationService : ISimulationService, IDisposable
     {
-        private Client _client;
-        private Thread _thread;
         private bool _running;
+        private Thread _worker;
 
         public void Start(uint appId)
         {
-            // инициализируем Facepunch-клиент как “игру” с любым AppID (можно 480)
-            _client = new Client(new AppId(105600)); 
-            // говорим Steam-клиенту, что мы играем в нужный appId
-            _client.Friends.SetPlayedGame(new PlayedGame(appId));
+            // 1) Инициализация
+            if (!SteamAPI.Init())
+                throw new InvalidOperationException("Не удалось инициализировать SteamAPI");
 
+            // 2) Устанавливаем “в игре” для нужного AppID
+            SteamFriends.SetPlayedGame(new FriendGameInfo_t
+            {
+                m_gameID = new CGameID(appId),
+                m_unGameIP = 0,
+                m_unGamePort = 0,
+                m_unSpectatorPort = 0,
+                m_SteamIDLobby = CSteamID.Nil
+            });
+
+            // 3) Фон для колбеков
             _running = true;
-            _thread = new Thread(() =>
+            _worker = new Thread(() =>
             {
                 while (_running)
                 {
-                    _client.Tick();
+                    SteamAPI.RunCallbacks();
                     Thread.Sleep(15_000);
                 }
             })
             { IsBackground = true };
-            _thread.Start();
+            _worker.Start();
         }
 
         public void Stop()
         {
             _running = false;
-            _thread?.Join();
-            // сбрасываем статус
-            _client.Friends.SetPlayedGame(new PlayedGame(0));
-            _client.Dispose();
+            _worker?.Join();
+            SteamFriends.ClearPlayedGame(); // сброс статуса
+            SteamAPI.Shutdown();
         }
+
+        public void Dispose() => Stop();
     }
 }
